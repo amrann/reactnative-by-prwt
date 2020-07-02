e57a25b2b9cc94e03285180c5cba96ee326fa3f3
@meran <amr.bin.an@gmail.com>
2020-07-02 08:02:26 +0700

    Style Component

--- App.js
+++ App.js
@@ -1,10 +1,31 @@
 import React, {Component} from 'react';
 import {Text, View, Image, TextInput} from 'react-native';
+import importMacbook from './macbook.jpg';
 
 const App = () => {
   // return <Text>Meran</Text>
   return (
     // Ingat, dalam return hanya bisa punya 1 induk doang
+    <StyleComponent/>
+  )
+}
+
+const StyleComponent = () => {
+  return(
+    <View style={{padding: 12, backgroundColor: 'white', width: 212, borderRadius: 8}}>
+      <Image source={importMacbook} style={{width: 188, height: 107, borderRadius: 8}}/>
+      <Text style={{fontSize: 14, fontWeight: 'bold', marginTop: 16}}>New Macbook Pro</Text>
+      <Text style={{fontSize: 12, fontWeight: 'bold', color: '#F2994A'}}>Rp 25.000.000</Text>
+      <Text style={{fontSize: 12, fontWeight: '300', marginTop: 12}}>Jakarta Barat</Text>
+      <View style={{backgroundColor: '#6FCF97', paddingVertical: 6, borderRadius: 25, marginTop: 20}}>
+        <Text style={{fontSize: 14, fontWeight: '600', color: 'white', textAlign: 'center'}}>Beli</Text>
+      </View>
+    </View>
+  )
+}
+
+const SammpleComponent = () => {
+  return(
     <View>
       <View
         // source backgroundColor = https://flatuicolors.com/palette/fr 
